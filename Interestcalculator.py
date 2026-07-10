8b21cee16fce5836a09bdb90913946faacb1be80
Aaryan Singh <aarya.thakur.up@gmail.com>
2026-07-10 07:50:52 +0530

    New comment added

--- Interestcalculator.py
+++ Interestcalculator.py
@@ -1,5 +1,7 @@
 # Interest Calculator Program 
 # This program calculates both simple and compound interest based on user input for principal, rate, and time.
+# The user is prompted to enter the principal amount, annual interest rate (in percentage), and time in years. The program then calculates the simple interest using the formula (P * R * T) / 100 and the compound interest using the formula P * ((1 + (R / (n * 100))) ** (n * T)) - P, where n is the number of times interest is compounded per year (assumed to be 1 for annual compounding). Finally, it displays the results, including the profit from compound interest compared to simple interest.
+# The program is structured using a class called InterestCalculator, which encapsulates the methods for calculating simple and compound interest, as well as displaying the results. The user inputs are collected at the end of the script, and an instance of the InterestCalculator class is created to perform the calculations and display the results.
 class InterestCalculator:
     def __init__(self, principal, rate, time):
         self.principal = principal
