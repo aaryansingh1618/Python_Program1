61213718e13394ae97ed641c91bfc5692f6778cc
Aaryan Singh <aarya.thakur.up@gmail.com>
2026-07-12 21:16:20 +0530

    Cart Program added

---
+++ Shopingcart.py
@@ -0,0 +1,19 @@
+foods = []
+prices = []
+total = 0
+while True:
+    food = (input("Enter your food to buy(q to quit)"))
+    if food.lower() == "q":
+     break
+    else: 
+     price = float(input(f"Enter the Price of {food}: $"))
+     foods.append(food)
+     prices.append(price)
+print("-----Your Cart-----")
+for food in foods:
+ print(food,end= "")
+ print()
+for price in prices:
+ total += price
+print(f"Your Total is: {total}$")
+    
