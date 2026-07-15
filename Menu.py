012f99c60775b5ac43c92a6b03ab6477e12c0abc
Aaryan Singh <aarya.thakur.up@gmail.com>
2026-07-15 07:31:35 +0530

    Menu Program Added

---
+++ Menu.py
@@ -0,0 +1,33 @@
+menu ={"Pizza": 250.00,
+       "Chole Bhature": 100,
+       "Chocolate" : 100,
+       "Momos": 50,
+       "Cold Drink": 50,
+       "Chips": 50,
+       "Popcorn": 100,
+       "Protein Shake": 100,
+       "Oat Meal": 100,
+       "Pineapple Juice": 50,
+       "Biryani": 100,
+       "Eggs": 100}
+cart = []
+total = 0
+print("\033[1m            MENU\033[0m")
+print("-------------------------------")
+for key,value in menu.items():
+ print(f"{key:20}  {value: .2f}")
+print("-------------------------------")
+while True:
+    food = input("Select an item (q to quit): ")
+    if food.lower() =="q":
+        break
+    elif menu.get(food) is not None:
+         cart.append(food)
+
+
+print("---------Your Order-----------")
+for food in cart:
+    total += menu.get(food)
+    print(food,end="   ")
+print()
+print(f"Total is: {total: .2f}")
